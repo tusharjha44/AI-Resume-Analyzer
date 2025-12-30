766ed17784f0c893ac9e9a18a53a6c8698eeb5b0
tushar-jha-vs <tushar.jha@vectoscalar.com>
2025-12-30 23:49:06 +0530

    Authentication

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,50 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import ResumeCard from "~/components/ResumeCard";
+import { resumes } from "../../constants";
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
+import { usePuterStore } from "lib/puter";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resumind" },
+    {
+      name: "description",
+      content:
+        "Resumind is a tool that helps you analyze your resume and get feedback on it.",
+    },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  const {
+    auth: { isAuthenticated },
+  } = usePuterStore();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      navigate("/auth?next=/");
+    }
+  }, [isAuthenticated]);
+
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar />
+      <section className="main-section">
+        <div className="page-heading py-16">
+          <h1>Track your Application & Resume Ratings</h1>
+          <h2>Review your resume and get AI powered feedback</h2>
+        </div>
+        {resumes?.length > 0 && (
+          <div className="resumes-section">
+            {resumes.map((resume: Resume) => (
+              <ResumeCard key={resume.id} resume={resume} />
+            ))}
+          </div>
+        )}
+      </section>
+    </main>
+  );
 }
