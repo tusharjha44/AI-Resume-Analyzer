647a6d40b860f1ac46296b319397bd0becc56f3f
tushar-jha-vs <tushar.jha@vectoscalar.com>
2025-12-31 00:13:58 +0530

    Cloud Storage and AI Feedback

---
+++ app/components/FileUploader.tsx
@@ -0,0 +1,88 @@
+import { formatSize } from "lib/utils";
+import { useCallback, useState } from "react";
+import { useDropzone } from "react-dropzone";
+
+interface FileUploaderProps {
+  onFileSelect?: (file: File | null) => void;
+}
+
+const FileUploader = ({ onFileSelect }: FileUploaderProps) => {
+  const [file, setFile] = useState<File | null>(null);
+
+  const onDrop = useCallback(
+    (acceptedFiles: File[]) => {
+      const selectedFile = acceptedFiles[0] || null;
+      setFile(selectedFile);
+      onFileSelect?.(selectedFile);
+    },
+    [onFileSelect]
+  );
+
+  const maxFileSize = 20 * 1024 * 1024; // 20MB in bytes
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop,
+    multiple: false,
+    accept: { "application/pdf": [".pdf"] },
+    maxSize: maxFileSize,
+  });
+
+  const handleRemove = useCallback(
+    (e: React.MouseEvent) => {
+      e.stopPropagation();
+      setFile(null);
+      onFileSelect?.(null);
+    },
+    [onFileSelect]
+  );
+
+  return (
+    <div className="w-full gradient-border">
+      <div {...getRootProps()}>
+        <input {...getInputProps()} />
+
+        <div className="space-y-4 cursor-pointer">
+          {file ? (
+            <div
+              className="uploader-selected-file"
+              onClick={(e) => e.stopPropagation()}
+            >
+              <img src="/images/pdf.png" alt="pdf" className="size-10" />
+              <div className="flex items-center space-x-3">
+                <div>
+                  <p className="text-sm font-medium text-gray-700 truncate max-w-xs">
+                    {file.name}
+                  </p>
+                  <p className="text-sm text-gray-500">
+                    {formatSize(file.size)}
+                  </p>
+                </div>
+              </div>
+              <button
+                type="button"
+                className="p-2 cursor-pointer"
+                onClick={handleRemove}
+              >
+                <img src="/icons/cross.svg" alt="remove" className="w-4 h-4" />
+              </button>
+            </div>
+          ) : (
+            <div>
+              <div className="mx-auto w-16 h-16 flex items-center justify-center mb-2">
+                <img src="/icons/info.svg" alt="upload" className="size-20" />
+              </div>
+              <p className="text-lg text-gray-500">
+                <span className="font-semibold">Click to upload</span> or drag
+                and drop
+              </p>
+              <p className="text-lg text-gray-500">
+                PDF (max {formatSize(maxFileSize)})
+              </p>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+export default FileUploader;
