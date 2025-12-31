413325d0c2f5a3feb99698222838a9551f6b8fda
tushar-jha-vs <tushar.jha@vectoscalar.com>
2025-12-31 13:02:54 +0530

    Resume Feedback screen and fetch real resumes

--- app/components/ResumeCard.tsx
+++ app/components/ResumeCard.tsx
@@ -1,9 +1,27 @@
-import React from "react";
 import { Link } from "react-router";
 import ScoreCircle from "./ScoreCircle";
+import { useEffect, useState } from "react";
+import { usePuterStore } from "../../lib/puter";
 
-const ResumeCard = ({ resume }: { resume: Resume }) => {
-  const { id, companyName, jobTitle, feedback, imagePath } = resume;
+const ResumeCard = ({
+  resume: { id, companyName, jobTitle, feedback, imagePath },
+}: {
+  resume: Resume;
+}) => {
+  const { fs } = usePuterStore();
+  const [resumeUrl, setResumeUrl] = useState("");
+
+  useEffect(() => {
+    const loadResume = async () => {
+      const blob = await fs.read(imagePath);
+      if (!blob) return;
+      let url = URL.createObjectURL(blob);
+      setResumeUrl(url);
+    };
+
+    loadResume();
+  }, [imagePath]);
+
   return (
     <Link
       to={`/resume/${id}`}
@@ -25,11 +43,11 @@ const ResumeCard = ({ resume }: { resume: Resume }) => {
           <ScoreCircle score={feedback.overallScore} />
         </div>
       </div>
-      {imagePath && (
+      {resumeUrl && (
         <div className="gradient-border animate-in fade-in duration-1000">
           <div className="w-full h-full">
             <img
-              src={imagePath}
+              src={resumeUrl}
               alt="resume"
               className="w-full h-[350px] max-sm:h-[200px] object-cover object-top"
             />
@@ -39,5 +57,4 @@ const ResumeCard = ({ resume }: { resume: Resume }) => {
     </Link>
   );
 };
-
 export default ResumeCard;
